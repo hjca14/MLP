47c21ee82ca732db66b296b502176bc7366835ee
hjca14 <hjca14@gmail.com>
2025-06-02 13:02:54 -0300

    arrumando lint

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import unittest
 from app import app
 
+
 class TestAPI(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
@@ -11,6 +12,7 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         data = response.get_json()
         self.assertIn('categoria', data)
+
 
 if __name__ == '__main__':
     unittest.main()
